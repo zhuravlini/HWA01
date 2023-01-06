2f165a2735163fe13efc6fe2212b3d4cf1e3ab76
Tatiana Zhuravleva <zhuravlini@gmail.com>
2023-01-06 14:09:31 +0300

    junit4

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,9 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+//        if ((amount % boundary) == 0) {
+//            return 0;
+//        }
         return boundary - amount % boundary;
     }
 }
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,24 +1,25 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainIfBoundary() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
